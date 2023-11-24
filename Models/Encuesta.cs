1efa11c83e243b60db23f65501bc40f3d41fbd60
dx006328 <daniel.guzman@maxar.com>
2023-11-24 10:28:21 -0600

    Se corrige el modelo de Encuesta para mapear correctamente el apellido

--- Models/Encuesta.cs
+++ Models/Encuesta.cs
@@ -3,17 +3,17 @@
 public class Encuesta
 {
     public string Nombre { get; set; }
-    public string Apellido { get; set; }
+    public string Apellidos { get; set; }
     public string Pais { get; set; }
     public string Rol { get; set; }
     public string LenguajePrimario { get; set; }
     public string LenguajeSecundario { get; set; }
 
-    public Encuesta(string nombre, string apellido, string pais, string rol,
+    public Encuesta(string nombre, string apellidos, string pais, string rol,
         string lenguajePrimario, string lenguajeSecundario)
     {
         Nombre = nombre;
-        Apellido = apellido;
+        Apellidos = apellidos;
         Pais = pais;
         Rol = rol;
         LenguajePrimario = lenguajePrimario;
